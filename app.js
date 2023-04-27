c65862668c166be94147c94cc7dbf658dbde7638
Derek <derekdanielson84@gmail.com>
2023-04-27 11:23:46 -0600

    Notes added

--- app.js
+++ app.js
@@ -34,6 +34,18 @@ function replaceAllItems(){
 }
 
 
+function replaceChildHeading(){
+    const header = document.querySelector('header');
+    const h1 = document.querySelector('header h1');
+
+    const h2 = document.createElement('h2');
+    h2.id = 'app-title';
+    h2.textContent = 'Shopping List';
+    header.replaceChild(h2, h1);
+}
+
+
 replaceFirstItem();
 replaceSecondItem();
 replaceAllItems();
+replaceChildHeading();
